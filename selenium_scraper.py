4a2643fbf55364d7205aad698ccd4880f3acceb7
admin <oluochcliffe13@gmail.com>
2026-02-24 12:58:35 +0000

    Initialize the web driver for selenium scraping

---
+++ selenium_scraper.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def get_phone_no(listing_url):
+	chrome_options = Options()
+	chrome_options.add_argument("--headless")
+	chrome_options.add_argument("--no-sandbox")
+	chrome_options.add_argument("--disable-dev-shm-usage")
+
+	# Launch the browser
+	driver = webdriver.Chrome(options=chrome_options)
+
