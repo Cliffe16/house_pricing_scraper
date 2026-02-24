937a182c61678bcd7f62217d1a0c33ed13c76672
admin <oluochcliffe13@gmail.com>
2026-02-24 09:45:17 +0000

    Configure field to be scraped from the site

---
+++ scraper.py
@@ -0,0 +1,59 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+# Define urls from the real estate listings
+url_rent = "https://www.buyrentkenya.com/houses-for-rent"
+#url_sale
+
+def scrape(url):
+	html = requests.get(url).text
+	soup = BeautifulSoup(html, "html.parser")
+
+	# Initialize empty list to contain the listings
+	listings = []
+
+	# Select listing cards
+	cards = soup.select("listing-card")
+
+	# Iterate through the listing cards
+	for card in cards:
+		# Listing id
+		listing_id = soup.select_one("div.search-listing-open-phone-modal[data-bi-listing-id]")
+		id = listing_id['data-bi-listing-id']
+
+		# Property description
+		listing_description = soup.select_one("h2.font-semibold")
+		listing_description = listing_description.text.strip() if listing_description else None
+
+		# Category
+		listing_category = soup.select_one("div.relative[data-bi-listing-category]")
+		category = listing_category['data-bi-listing-category']
+
+		# Number of bedrooms
+		bedroom_count = soup.select_one("span.whitespace-nowrap[data-cy='card-bedroom_count']")
+		bedroom_count = bedroom_count.text.strip() if bedroom_count else None
+
+		# Number of bathrooms
+		bathroom_count = soup.select_one("span.whitespace-nowrap[data-cy='card-bathroom_count']")
+		bathroom_count = bathroom_count.text.strip() if bathroom_count else None
+
+		# Offer Type
+		offer_type = soup.select_one("div.search-listing-open-phone-modal[data-bi-listing-offer-type]")
+		offer_type = offer_type['data-bi-listing-offer-type"]
+
+		# Location
+		location = soup.select_one("p.w-full")
+		location = location.text.strip() if location else None
+
+		# Listing Price
+		listing_price = soup.select_one("div.relative[data-bi-listing-price]")
+		listing_price = listing_price['data-bi-listing-price']
+
+		# Agency
+		agency = soup.select_one("div.search-listing-open-phone-modal[data-bi-listing-agent]")
+		agency = agency["data-bi-listing-agent"]
+
+		# Listing link
+		url = soup.select_one("a.absolute")
+		url = url.text.strip() if url else None
