37ecf75512272226906839b3ebf10096ce309216
admin <oluochcliffe13@gmail.com>
2026-02-26 09:48:13 +0000

    Fix order of operations bug

--- airflow/dags/housing_data.py
+++ airflow/dags/housing_data.py
@@ -3,7 +3,6 @@ from airflow import DAG
 from datetime import datetime, timedelta
 import sys
 import os
-from dotenv import load_dotenv
 
 # Trace the symlink back to the actual project folder dynamically
 current_file_path = os.path.realpath(__file__)
@@ -11,14 +10,15 @@ dags_folder = os.path.dirname(current_file_path)
 airflow_folder = os.path.dirname(dags_folder)
 project_root = os.path.dirname(airflow_folder)
 
-# Load the .env file using the dynamic root path
-load_dotenv(os.path.join(project_root, '.env'))
-
 # Inject the paths dynamically
 sys.path.append(project_root)
 sys.path.append(os.path.join(project_root, 'venv/lib/python3.12/site-packages'))
 
+from dotenv import load_dotenv
 from main_scraper import run_pipeline
+
+# Load the .env file using the dynamic root path
+load_dotenv(os.path.join(project_root, '.env'))
 
 with DAG(
 	"housing_scraper",
